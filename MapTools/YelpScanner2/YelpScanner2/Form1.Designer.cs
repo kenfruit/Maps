0812fd933069a7b32c1d1eb39f14dcef70215038
Ken Fruit <ken.fruit@gmail.com>
2015-12-19 07:00:58 -0600

    Added YelpScanner2 to GitHub

---
+++ MapTools/YelpScanner2/YelpScanner2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace YelpScanner2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetHours = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetHours
+            // 
+            this.btnGetHours.Location = new System.Drawing.Point(277, 533);
+            this.btnGetHours.Name = "btnGetHours";
+            this.btnGetHours.Size = new System.Drawing.Size(75, 23);
+            this.btnGetHours.TabIndex = 0;
+            this.btnGetHours.Text = "Get Hours";
+            this.btnGetHours.UseVisualStyleBackColor = true;
+            this.btnGetHours.Click += new System.EventHandler(this.btnGetHours_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(13, 13);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(339, 20);
+            this.tbInput.TabIndex = 1;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(13, 40);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(339, 475);
+            this.tbOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 559);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnGetHours);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetHours;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+    }
+}
+
